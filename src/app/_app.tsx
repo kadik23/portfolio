ecbcc7d1d9020744aa4ce7660188b43854d288dd
unknown <kadiksalah03@gmail.com>
2024-06-03 14:59:56 +0100

    Fixed some issues

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Inika:wght@400;700&family=Rubik+Scribble&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
