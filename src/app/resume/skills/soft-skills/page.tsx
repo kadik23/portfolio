5a8dcb7bb5b5fbaf6367e5c077f4beb3c5e51d4f
unknown <kadiksalah03@gmail.com>
2024-06-07 16:40:56 +0100

    Create soft skills component

--- src/app/resume/skills/soft-skills/page.tsx
+++ src/app/resume/skills/soft-skills/page.tsx
@@ -7,15 +7,15 @@ function page() {
   return (
     <div className='lg:w-5/6 '>
       <div className='flex flex-wrap lg:justify-start gap-8 justify-center my-4'>
-        {softSkills.map((softSkill) =>(
-          <SoftSkill percentage={softSkill.percentage} name={softSkill.name}/>
+        {softSkills.map((softSkill, index) =>(
+          <SoftSkill key={index} percentage={softSkill.percentage} name={softSkill.name}/>
         ))}
       </div>
       <div className='flex flex-col items-center lg:items-start my-8'>
         <div className='text-xl inika-bold'>Languages</div>
         <div className='my-8 lg:my-2 w-full lg:w-5/6 flex flex-col gap-4 '>
-          {languages.map((language) =>(
-            <Language name={language.name} percentage={language.percentage} />
+          {languages.map((language, index) =>(
+            <Language key={index} name={language.name} percentage={language.percentage} />
           ))}
         </div>
       </div>
