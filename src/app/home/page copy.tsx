0c058023a86e8d39d2e847e6acb528df5a4bfe63
unknown <kadiksalah03@gmail.com>
2024-06-10 16:49:55 +0100

    Fixed some errors

--- src/app/home/page copy.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: "experience",
-};
-
-export default function experiencepage(){
-    return (<>experience Page</>)
-}
