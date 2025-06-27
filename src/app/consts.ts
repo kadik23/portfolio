9089a6fb60ce54c168ea2bdc90eb7e9d9ad0cb70
kadik23 <kadiksalah03@gmail.com>
2025-06-27 15:56:42 +0100

    update data

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -78,7 +78,7 @@ export const projects: Projects[] = [
         id: 1,
         title:'Coding Challenges  & Hiring Platform',
         description: '     A Coding Challenges & Hiring Platform connects developers and companies through skill-based challenges, facilitating efficient hiring and learning.',
-        frameworks: [ 'React Js', 'Type Script', 'Tailwind Css', 'Express Js', 'My sql', 'Docker'],
+        frameworks: [ 'Layered Architecture','React Js', 'Type Script', 'Tailwind Css', 'Express Js', 'My sql', 'Docker'],
         deployments: 'https://code-craft-front.vercel.app/',
         code_source: 'https://github.com/code-craft-project',
         image:'01.png' 
@@ -87,7 +87,7 @@ export const projects: Projects[] = [
         id: 2,
         title:'Novalance',
         description: 'A Novalance is an online platform that connects freelancers with clients who need their services. These websites provide a space where freelancers can showcase their skills, and clients can post projects or tasks that they need help with.',
-        frameworks: [ 'Vue Js', 'Laravel', 'Tailwind Css', 'Node Js', 'Web Socket','Docker'],
+        frameworks: [ 'Hexagonal Architecture','Vue Js', 'Laravel', 'Tailwind Css', 'Node Js', 'Web Socket','Docker'],
         deployments: 'https://novalance-frontend.vercel.app/',
         code_source: 'https://github.com/kadik23/Novalance?tab=readme-ov-file',
         image:'02.png' 
@@ -96,7 +96,7 @@ export const projects: Projects[] = [
         id: 3,
         title:'Stanissk Store',
         description: 'Stanissk Store is eCommerce web app, online platform that allows users to browse, search, and purchase products or services over the internet with online payment feature.',
-        frameworks: [ 'Laravel','Vue Js','Type Script','Blade', 'Tailwind Css', 'My Sql','Docker','Pusher'],
+        frameworks: [ 'Hexagonal & MVC Architecture','Laravel','Vue Js','Type Script','Blade', 'Tailwind Css', 'My Sql','Docker','Pusher'],
         deployments: 'https://ecommerce-app-chi-eight.vercel.app/',
         code_source: 'https://github.com/kadik23/ecommerce-app',
         image:'03.png' 
@@ -105,7 +105,7 @@ export const projects: Projects[] = [
         id: 4,
         title:'Zenstay',
         description: '  Zenstay is an online platform that allows users to reserve rooms for various purposes with online payment methods.',
-        frameworks: [ 'Mern Stack','Bootstrap','Zustand'],
+        frameworks: [ 'MVC Architecture','Mern Stack','Bootstrap','Zustand'],
         deployments: 'https://zenstay-two.vercel.app/',
         code_source: 'https://github.com/kadik23/Zenstay',
         image:'04.png' 
@@ -114,7 +114,7 @@ export const projects: Projects[] = [
         id: 5,
         title:'Doctor Appointment',
         description: 'A Web Application to make an appointment with Doctors easily. ',
-        frameworks: [ 'Mern Stack','Tailwind Css', 'Type Script', 'Prisma',],
+        frameworks: [ 'Modular Monolith Architecture','Mern Stack','Tailwind Css', 'Type Script', 'Prisma',],
         deployments: "https://doctor-appointment-5-btf0.onrender.com/home",
         code_source: 'https://github.com/kadik23/doctor-appointment',
         image:'05.png' 
@@ -123,7 +123,7 @@ export const projects: Projects[] = [
         id: 6,
         title:'Cs Department Management',
         description: 'Web application to manage the computer science department like Adminstraters, Teachers and Students',
-        frameworks: [ 'Laravel','React Js','Inertia','My Sql', 'Redux', 'TailwindCss'],
+        frameworks: [ 'MVC','Laravel','React Js','Inertia','My Sql', 'Redux', 'TailwindCss'],
         deployments: null,
         code_source: 'https://github.com/kadik23/cs-department-management',
         image:'07.png' 
@@ -132,7 +132,7 @@ export const projects: Projects[] = [
         id: 7,
         title:'Law Firm',
         description: 'Web application to share files between lawyers and clients and blog and services feature with online payments',
-        frameworks: [ 'Next JS','Express Js', 'React-Hook-Form','Tailwind','My Sql',],
+        frameworks: [ 'MVC Architecture','Next JS','Express Js', 'React-Hook-Form','Tailwind','My Sql',],
         deployments: 'https://law-site-beryl.vercel.app/',
         code_source: 'https://github.com/kadik23/law-firm-web-app',
         image:'09.png' 
@@ -141,7 +141,7 @@ export const projects: Projects[] = [
         id: 8,
         title:'Banquet Hall Management',
         description: 'Desktop application to manage the Banquet Hall like Clients, Reservations, Paiments and Receipts',
-        frameworks: [ 'Electron JS','React JS','Css','Sqlite',],
+        frameworks: [ 'MVC Architecture', 'Electron JS','React JS','Css','Sqlite',],
         deployments: null,
         code_source: 'https://github.com/kadik23/banquet-hall-management',
         image:'08.png' 
